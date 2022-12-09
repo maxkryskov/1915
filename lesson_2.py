51bc9cc7f776b3adea4faf4fc40fc5c32d541b9e
Admin <kriskovmaksim6@gmail.com>
2022-12-09 17:06:58 +0200

    lesson_2.py. Class and object class

---
+++ lesson_2.py
@@ -0,0 +1,27 @@
+class Student:
+    print("Hi")
+    def __init__(self):
+        self.height = 170
+        print("I am alive! ")
+        self.age = "10"
+        self.color = "green"
+        self.ok = "okey"
+    def print(self):
+        (self.height)
+        (self.age)
+        (self.color)
+        (self.ok)
+Yaroslav = Student()
+Alexandra = Student()
+Max = Student()
+Nazar = Student()
+# print(Nazar.height)
+# print(Max.height)
+# print(Yaroslav.height)
+# print(Alexandra.height)
+# Yaroslav.height = 159
+# Alexandra.height = 157
+# Max.height = 164
+# Nazar.height = 168
+# print(Yaroslav.height)
+# print(Alexandra.height)
